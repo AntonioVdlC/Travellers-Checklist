b2af42e2c1ed9325e476cb8ebf347584b3f116f2
AntonioVdlC <antonio.villagra.de.la.cruz@gmail.com>
2013-10-12 20:26:34 +0200

    Update router, now routing to checklistPage.

--- js/app/router.js
+++ js/app/router.js
@@ -1,17 +1,20 @@
 define(function (require){
 
-	var $ 			= require('jquery'),
-		Backbone	= require('backbone'),
-		//Store		= require('app/store/websql-store'),
-		HomeView 	= require('app/views/home.view'),
+	var $ 				= require('jquery'),
+		Backbone		= require('backbone'),
+		//Store			= require('app/store/websql-store'),
+		HomeView 		= require('app/views/home.view'),
+		CheckListView	= require('app/views/categoryList.view'),
 
-		homeView = new HomeView();
+		homeView = new HomeView(),
+		checkListView;
 
 	return Backbone.Router.extend({
 
 		routes: {
 			'' : 'home',
-			'checklist/:id': 'checkListPage'
+			'checklist/:id': 'checkListPage',
+			'checklist/:clId/category/:catId': 'categoryPage'
 		},
 
 		home: function (){
@@ -20,7 +23,15 @@ define(function (require){
 
 		checkListPage: function (id) {
 			console.log('Routing to CheckList Page: ' + id);
-		}
+			checkListView = new CheckListView({el:$('#mainContent')});
+			
+			checkListView.id = id;
+			//console.log(checkListView);
+			checkListView.preRender();
+		},
 
+		categoryPage: function (clId, catId) {
+			console.log('Routing to Category Page: ' + catId + ' from CheckList: ' + clId);
+		}
 	});
 });
