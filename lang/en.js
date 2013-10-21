262892d5a846a4face7f4ed1a18758c04cf8e61f
AntonioVdlC <antonio.villagra.de.la.cruz@gmail.com>
2013-10-21 19:11:58 +0200

    English version.

--- lang/en.js
+++ lang/en.js
@@ -1,23 +1,49 @@
 var lang = {
 //A
+	"add": "Add",
 //B
+	"blank": "Blank",
 //C
+	"cancel": "Cancel",
+	"category": "category",
+	"Category": "Category",
+	"checkedOn": "Checked on: ",
+	"checklist": "checklist",
+	"Checklist": "Checklist",
+	"create": "Create",
 //D
+	"default": "Default",
+	"delete": "Delete",
+	"deleteConfirm": "Are you sure you want to delete the ",
 //E
 //F
 //G
 //H
+	"helloWorld": "Hello World!",
 //I
+	"introText": "Welcome to Traveller's Checklist!<br/>You can easily manage your checklists before going on a trip, so you will never again forget a thing!",
+	"item": "item",
+	"Item": "Item",
+	"items": "item(s)",
 //J
 //K
 //L
+	"lastModified": "Last modified on: ",
 //M
+	"model": "model",
+	"Model": "Model",
 //N
+	"name": "Name",
+	"nameUnique": "The name must be unique within the app.",
+	"new": "New",
+	"newChecklist": "New Checklist",
 //O
+	"OK": "OK",
 //P
 //Q
 //R
 //S
+	"saveModel": "Save as Model"
 //T
 //U
 //V
