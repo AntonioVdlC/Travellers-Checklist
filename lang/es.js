72173a9f8bfe392b7985e851b34bede02d2fa7aa
AntonioVdlC <antonio.villagra.de.la.cruz@gmail.com>
2013-10-21 19:48:19 +0200

    Spanish version.

--- lang/es.js
+++ lang/es.js
@@ -1,23 +1,53 @@
 var lang = {
 //A
+	"add": "Añadir",
 //B
+	"blank": "En blanco",
 //C
+	"cancel": "Cancelar",
+	"category": "categoría",
+	"Category": "Categoría",
+	"checkedOn": "Chequeado: ",
+	"checklist": "lista",
+	"Checklist": "Lista",
+	"create": "Crear",
 //D
+	"default": "Por defecto",
+	"delete": "Supprimir",
+	"deleteConfirmCat": "¿Estás seguro de que quieres borrar la ",
+	"deleteConfirmCL": "¿Estás seguro de que quieres borrar la ",
+	"deleteConfirmItem": "¿Estás seguro de que quieres borrar el ",
 //E
 //F
 //G
 //H
+	"helloWorld": "Hello World!",
 //I
+	"introText": "¡Bienvenido al Traveller's Checklist!<br/>Ahora puedes gestinar tus listas de viaje antes de irte de viaje, de tal manera que ya no se te olvidará nada.",
+	"item": "objeto",
+	"Item": "objeto",
+	"items": "objeto(s)",
 //J
 //K
 //L
+	"lastModified": "Última modificación: ",
 //M
+	"model": "modelo",
+	"Model": "Modelo",
 //N
+	"name": "Nombre",
+	"nameUnique": "El nombre tiene que ser único dentro de la aplicación.",
+	"new": "Nuevo",
+	"newCategory": "Nueva categoría ...",
+	"newChecklist": "Nueva lista",
+	"newItem": "Nuevo objeto ...",
 //O
+	"OK": "OK",
 //P
 //Q
 //R
 //S
+	"saveModel": "Guardar como Modelo"
 //T
 //U
 //V
