62947acab87c7e4acaab094bcd2fba25789dafa2
AntonioVdlC <antonio.villagra.de.la.cruz@gmail.com>
2013-10-23 00:07:43 +0200

    Language support for Default model.

--- lang/en.js
+++ lang/en.js
@@ -1,15 +1,31 @@
 var lang = {
 //A
-	"add": "+",
+	"adapter": "Adapter",
+	"add": "Add",
+	"alarmClock": "Alarm Clock",
+	"antiemetic": "Antiemetic",
+	"antidiarrhoeal": "Antidiarrhoeal",
+	"antihistaminic": "Antihistaminic",
+	"antisepticCream": "Antiseptic Cream",
 //B
+	"bandages": "Bandages",
+	"belt": "Belt",
+	"bermudas": "Bermudas",
 	"blank": "Blank",
 //C
+	"camera": "Camera",
 	"cancel": "Cancel",
+	"cap": "Cap",
 	"category": "category",
 	"Category": "Category",
+	"chargers": "Chargers",
 	"checkedOn": "Checked on: ",
 	"checklist": "checklist",
 	"Checklist": "Checklist",
+	"cholera": "Cholera",
+	"clothes": "Clothes",
+	"compass": "Compass",
+	"cosmetics": "Cosmetics",
 	"create": "Create",
 //D
 	"default": "Default",
@@ -17,23 +33,46 @@ var lang = {
 	"deleteConfirmCat": "Are you sure you want to delete the ",
 	"deleteConfirmCL": "Are you sure you want to delete the ",
 	"deleteConfirmItem": "Are you sure you want to delete the ",
+	"deodorant": "Deodorant",
+	"diphtheria": "Diphtheria",
+	"drivingLicense": "Driving License",
 //E
+	"electronics": "Electronics",
+	"essentials": "Essentials",
 //F
+	"firstAidKit": "1st Aid Kit",
+	"flashlight": "Flashlight",
 //G
+	"gourd": "Gourd",
 //H
 	"helloWorld": "Hello World!",
+	"hepatitisA": "Hepatitis A",
+	"hepatitisB": "Hepatitis B",
 //I
 	"introText": "Welcome to Traveller's Checklist!<br/>You can easily manage your checklists before going on a trip, so you will never again forget a thing!",
+	"influenza": "Influenza",
+	"insurance": "Insurance",
 	"item": "item",
 	"Item": "Item",
 	"items": "item(s)",
 //J
+	"jacket": "Jacket",
+	"japaneseEncephalitis": "Japanese Encephalitis",
 //K
 //L
+	"laptop": "Laptop",
 	"lastModified": "Last modified on: ",
+	"laxative": "Laxative",
+	"lighter": "Lighter",
+	"lock": "Lock",
+	"luckyCharms": "Lucky Charms",
 //M
+	"medicalKit": "Medical Kit",
+	"memoryCard": "Memory Card",
+	"mobile": "Mobile",
 	"model": "model",
 	"Model": "Model",
+	"multiPlug": "Multi Plug",
 //N
 	"name": "Name",
 	"nameUnique": "The name must be unique within the app.",
@@ -43,17 +82,49 @@ var lang = {
 	"newItem": "New item...",
 //O
 	"OK": "OK",
+	"others": "Others",
 //P
+	"pants": "Pants",
+	"paperwork": "Paperwork",
+	"paracetamol": "Paracetamol",
+	"passport": "Passport",
+	"phloroglucinol": "Phloroglucinol",
+	"photocopies": "Photocopies",
+	"pijamas": "Pijamas",
+	"poliomyelitis": "Poliomyelitis",
 //Q
 //R
+	"rabies": "Rabies",
 //S
 	"saveModel": "Save as Model",
+	"scarf": "Scarf",
+	"shampoo": "Shampoo",
+	"shoes": "Shoes",
+	"sleepingBag": "Sleeping Bag",
+	"soap": "Soap",
+	"socks": "Socks",
+	"swimsuit": "Swimsuit",
+	"swissArmyKnife": "Swiss Army Knife",
 //T
+	"tablet": "Tablet",
+	"tetanus": "Tetanus",
+	"tickets": "Plane/Train Tickets",
+	"toiletry": "Toiletry",
+	"toothbrush": "Toothbrush",
+	"toothpaste": "Toothpaste",
+	"towel": "Towel",
+	"tshirts": "T-shirts",
+	"tuberculosis": "Tuberculosis",
+	"typhoidFever": "Typhoid Fever",
 //U
+	"underwear": "Underwear",
 //V
+	"vaccines": "Vaccines",
+	"visas": "Visas",
 //W
 //X
 //Y
+	"yellowFever": "Yellow Fever",
 //Z
 //1-9
 //#
--- lang/es.js
+++ lang/es.js
@@ -1,15 +1,31 @@
 var lang = {
 //A
+	"adapter": "Adaptador",
 	"add": "Añadir",
+	"alarmClock": "Despertador",
+	"antiemetic": "Antiemético",
+	"antidiarrhoeal": "Antidiarreico",
+	"antihistaminic": "Antihistamínico",
+	"antiseptic Cream": "Crema Antiséptica",
 //B
+	"bandages": "Vendajes",
+	"bermudas": "Bermudas",
+	"belt": "Cinturón",
 	"blank": "En blanco",
 //C
+	"camera": "Cámara",
 	"cancel": "Cancelar",
+	"cap": "Sombrero",
 	"category": "categoría",
 	"Category": "Categoría",
+	"charges": "Cargadores",
 	"checkedOn": "Chequeado: ",
 	"checklist": "lista",
 	"Checklist": "Lista",
+	"cholera": "Cólera",
+	"clothes": "Ropa",
+	"compass": "Brújula",
+	"cosmetics": "Cosméticos",
 	"create": "Crear",
 //D
 	"default": "Por defecto",
@@ -17,23 +33,46 @@ var lang = {
 	"deleteConfirmCat": "¿Estás seguro de que quieres borrar la ",
 	"deleteConfirmCL": "¿Estás seguro de que quieres borrar la ",
 	"deleteConfirmItem": "¿Estás seguro de que quieres borrar el ",
+	"deodorant": "Desodorante",
+	"diphtheria": "Difteria",
+	"drivingLicense": "Carnet de Conducir",
 //E
+	"electonics": "Electrónica",
+	"essentials": "Esenciales",
 //F
+	"firstAidKit": "Kit Primeros Auxilios",
+	"flashlight": "Linterna",
 //G
+	"gourd": "Cantimplora",
 //H
 	"helloWorld": "Hello World!",
+	"hepatitisA": "Hepatitis A",
+	"hepatitisB": "Hepatitis B",
 //I
 	"introText": "¡Bienvenido al Traveller's Checklist!<br/>Ahora puedes gestinar tus listas de viaje antes de irte de viaje, de tal manera que ya no se te olvidará nada.",
+	"influenza": "Gripe",
+	"insurance": "Seguro",
 	"item": "objeto",
 	"Item": "objeto",
 	"items": "objeto(s)",
 //J
+	"jacket": "Chaqueta",
+	"japaneseEncephalitis": "Encefalitis Japonesa",
 //K
 //L
+	"laptop": "Ordenador Portátil",
 	"lastModified": "Última modificación: ",
+	"laxative": "Laxativo",
+	"lighter": "Mechero",
+	"lock": "candado",
+	"luckyCharms": "Amuletos de la Suerte",
 //M
+	"medicalKit": "Botiquín",
+	"memoryCard": "Tarjeta de Memoria",
+	"mobile": "Móvil",
 	"model": "modelo",
 	"Model": "Modelo",
+	"multiPlug": "Enchufe Múltiple",
 //N
 	"name": "Nombre",
 	"nameUnique": "El nombre tiene que ser único dentro de la aplicación.",
@@ -43,17 +82,49 @@ var lang = {
 	"newItem": "Nuevo objeto ...",
 //O
 	"OK": "OK",
+	"others": "Otros",
 //P
+	"pants": "Pantalones",
+	"paperwork": "Documentación",
+	"paracetamol": "Paracetamol",
+	"passport": "Pasaporte",
+	"phloroglucinol": "Floroglucinol",
+	"photocopies": "Fotocopias",
+	"pijamas": "Pijama",
+	"poliomyelitis": "Poliomielitis",
 //Q
 //R
+	"rabies": "Rabia",
 //S
 	"saveModel": "Guardar como Modelo",
+	"scarf": "Bufanda",
+	"shampoo": "Shampoo",
+	"shoes": "Zapatos",
+	"sleepingBag": "Saco de Dormir",
+	"soap": "Jabón",
+	"socks": "Calcetines",
+	"swimsuit": "Bañador",
+	"swissArmyKnife": "Navaja Suiza",
 //T
+	"tablet": "Tableta",
+	"tetanus": "Tétanos",
+	"tickets": "Billetes de avión/tren",
+	"toiletry": "Artículos de aseo",
+	"toothbrush": "Cepillo de Dientes",
+	"toothpaste": "Pasta de Dientes",
+	"towel": "Toalla",
+	"tshirts": "Camisetas",
+	"tuberculosis": "Tuberculosis",
+	"typhoidFever": "Fiebre Tifoidea",
 //U
+	"underwear": "Ropa Interior",
 //V
+	"vaccines": "Vacunas",
+	"visas": "Visados",
 //W
 //X
 //Y
+	"yellowFever": "Fiebre Amarilla",
 //Z
 //1-9
 //#
